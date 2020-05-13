861d40f0ce49f62d6a7fc638a6ccf0b5c5347ecd
Dominick Bruno <dbruno@unseen.is>
2020-05-13 13:43:45 -0400

    MVP part 1 player and room pies

--- src/player.py
+++ src/player.py
@@ -1,2 +1,15 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, current_room, inventory):
+        self.name = name
+        self.current_room = current_room
+        self.inventory = inventory
+
+        def __str__(self):
+            return f'{self.name}\'s location: {self.current_room}'
+
+        def move(self, direction): 
+            if getattr(self.current_room, f"{direction}") is not None: 
+                self.current_room = getattr(self.current_room, f"{direction}") 
+            else: print("------- sorry, you cannot move in that direction") 
